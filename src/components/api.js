ae0c7a6e79a8c8cb0d6a592620f131cf948a395c
mikeg <m.gusiev@svitla.com>
2018-09-05 14:15:05 +0300

    code refactoring for apo

--- src/components/api.js
+++ src/components/api.js
@@ -3,40 +3,40 @@ let myHeaders = new Headers();
 myHeaders.append('Content-Type', 'application/json');
 
 export const api = {
-    getUsers: () => {
-    return fetch(apiUrl + '/api/users',{
-      method: 'GET'
-    })
+  getUsers: () => {
+    return fetch(apiUrl + '/api/users', {
+      method: 'GET',
+    });
   },
   loginValidation: (userData) => {
-  return fetch(apiUrl + '/api/users/',
-    {
-      method: 'POST',
-      headers: myHeaders,
-      body: JSON.stringify(userData)
-    })
-},
-registrationValidation: (userData) => {
-  return fetch(apiUrl + '/api/users/',
-    {
-      method: 'PUT',
-      headers: myHeaders,
-      body: JSON.stringify(userData)
-    })
-},
-deleteUser: (id) => {
-  return fetch(apiUrl + '/api/users/' + id,
-    {
-      method: 'DELETE'
-    })
-},
-changeUserProps: (userData, id) => {
-  return fetch(apiUrl + '/api/users/' + id,
-    {
-      method: 'PUT',
-      headers: myHeaders,
-      body: JSON.stringify(userData)
-    })
-}
+    return fetch(apiUrl + '/api/users',
+      {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(userData),
+      });
+  },
+  registrationValidation: (userData) => {
+    return fetch(apiUrl + '/api/users',
+      {
+        method: 'PUT',
+        headers: myHeaders,
+        body: JSON.stringify(userData),
+      });
+  },
+  deleteUser: (id) => {
+    return fetch(apiUrl + '/api/users/' + id,
+      {
+        method: 'DELETE',
+      });
+  },
+  changeUserProps: (userData, id) => {
+    return fetch(apiUrl + '/api/users/' + id,
+      {
+        method: 'PUT',
+        headers: myHeaders,
+        body: JSON.stringify(userData),
+      });
+  },
 };
 
